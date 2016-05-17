e7d44010b887b8a5cdb85514e0901d57041c60c4
Kunal Bhambhani <kunal@parsus.com>
2016-05-17 00:29:50 -0700

    refine chart options to make it look similar to mockup

--- www/js/directives.js
+++ www/js/directives.js
@@ -13,25 +13,35 @@ angular.module('starter')
                               '', '', '6PM', '', '', '9PM', '', ''];
 
             // Highcharts.setOptions({
-            //     chart: {
-            //         backgroundColor: {
-            //             linearGradient: [0, 0, 500, 500],
-            //             stops: [
-            //                 [0, 'rgb(255, 255, 255)'],
-            //                 [1, 'rgb(240, 240, 255)']
-            //                 ]
-            //         },
-            //         borderWidth: 2,
-            //         plotBackgroundColor: 'rgba(255, 255, 255, .9)',
-            //         plotShadow: true,
-            //         plotBorderWidth: 1
+            //   chart: {
+            //     backgroundColor: {
+            //         linearGradient: [0, 0, 500, 500],
+            //         stops: [
+            //             [0, 'rgb(255, 255, 255)'],
+            //             [1, 'rgb(240, 240, 255)']
+            //             ]
+            //     },
+            //     borderWidth: 2,
+            //     color: {
+            //         linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+            //         stops: [
+            //             [0, '#003399'],
+            //             [1, '#3366AA']
+            //         ]
             //     }
+            //     plotBackgroundColor: 'rgba(255, 255, 255, .9)',
+            //     plotShadow: true,
+            //     plotBorderWidth: 1
+            //   }
             // });
 
             Highcharts.chart(element[0], {
-
+              credits: {
+                enabled: false
+              },
               series: [{
-                  data: scope.data
+                  data: scope.data,
+                  showInLegend: false
               }],
               chart: {
                   // height: '600px',
@@ -44,6 +54,15 @@ angular.module('starter')
               title: {
                   text: 'Mon, Feb 1, 2016'
               },
+              subtitle: {
+                style: {
+                  'font-size': '22px'
+                },
+                verticalAlign: 'middle',
+                text: '<b>198</b><span style="color: gray;font-size: 14px"> kWh</span><br/><span style="color: gray;font-size: 14px">total usage</span>',
+                x: 0,
+                y: 20
+              },
               pane: {
                   size: '85%'
               },
@@ -54,42 +73,70 @@ angular.module('starter')
               //     layout: 'vertical'
               // },
               xAxis: {
+                  distance: 5,
+                  gridLineColor: '#ffffff',
+                  lineColor: '#eeeeee',
                   min: 0,
                   max: 360,
+                  showFirstLabel: true,
                   type: "",
                   tickInterval: 15,
                   tickmarkPlacement: 'on',
                   labels: {
-                      formatter: function () {
-                          return categories[this.value / 15];
-                      }
+                    formatter: function () {
+                        return categories[this.value / 15];
+                    },
+                    backgroundColor: {
+                        linearGradient: [0, 0, 500, 500],
+                        stops: [
+                            [0, 'rgb(255, 255, 255)'],
+                            [1, 'rgb(120, 240, 120)']
+                            ]
+                    },
+                    style: {"color":"#333333"}
                   }
               },
               yAxis: {
-                  min: 0,
-                  endOnTick: false,
-                  showLastLabel: false,
-                  title: {
-                      text: '198 kWH total usage'
-                  },
-                  // labels: {
-                  //     formatter: function () {
-                  //         return this.value + '%';
-                  //     }
-                  // },
-                  reversedStacks: false
+                alternateGridColor: '#e4f1f5',
+                lineColor: '#ff0000',
+                gridLineColor: '#e4f1f5',
+                min: 0,
+                max: 0,
+                maxColor: '#ff0000',
+                endOnTick: true,
+                plotLines: [{color: '#ffffff'}],
+                showLastLabel: false,
+                reversedStacks: false,
+                startOnTick: true,
+                style: {"color": '#214fb0'}
               },
               // tooltip: {
               //     valueSuffix: '%'
               // },
               plotOptions: {
+                  column: {
+                    // color: '#0179c5',
+                    // colorByPoint: false
+                    zones: [{
+                      value: 0.3,
+                      color: '#0179c5'
+                    },{
+                      color: '#f054eb'
+                    }]
+                  },
                   series: {
                       stacking: 'normal',
                       shadow: false,
                       groupPadding: 0,
-                      pointPlacement: 'on'
+                      pointPlacement: 'between'
                   }
               }
+            }, function(chart) {
+              angular.forEach(chart.xAxis[0].ticks, function(tick, value) {
+                if (tick.label) {
+                  tick.label.attr({rotation: tick.pos});
+                }
+              });
             });
           }
       };
--- www/js/usage-controller.js
+++ www/js/usage-controller.js
@@ -6,7 +6,7 @@ angular.module('starter')
     // code to create a data set that looks like data: [[5, 2], [6, 3], [8, 2]]
     var windDirection, windSpeed, windDirectionJSON, windSpeedJSON, windDataJSON;
     windDirection = "[0,15,30,45,60,75,90,105,120,135,150,165,180,195,210,225,240,255,270,285,300,315,330,345,360]";
-    windSpeed = "[3,2,1,0,0,3,6,9,12,11,12,10,9,10,8,10,2,4,6,8,10,12,10,8]";
+    windSpeed = "[0.2,0.1,0.05,0.1,0,0.1,0.2,0.25,0.3,0.35,0.3,0.25,0.2,0.1,0.2,0.1,0.2,0.25,0.3,0.2,0.25,0.3,0.2,0.1]";
     windDirectionJSON = JSON.parse(windDirection);
     windSpeedJSON = JSON.parse(windSpeed);
     windDataJSON = [];
