cbe9220b07547912bff7a199c584fe072b350f82
Rahul Patel <rahul.patel@parsus.com>
2016-05-17 11:18:11 -0700

    feat(outages): add some outages code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,11 +17,47 @@ angular.module('starter.controllers', [])
   // };
 })
 
-.controller('OutagesCtrl', function($scope, $stateParams, Chats) {
-  // $scope.chat = Chats.get($stateParams.chatId);
+.controller('OutagesCtrl', function($scope, $cordovaGeolocation) {
+
+  // see http://www.joshmorony.com/integrating-google-maps-with-an-ionic-application/
+  var options = {timeout: 10000, enableHighAccuracy: true};
+
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    //Wait until the map is loaded
+    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+
+      var marker = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: latLng
+      });
+
+      var infoWindow = new google.maps.InfoWindow({
+          content: "0 outages reported!"
+      });
+
+      google.maps.event.addListener(marker, 'click', function () {
+          infoWindow.open($scope.map, marker);
+      });
+
+    });
+
+  }, function(error){
+    console.log("Could not get location");
+  });
 })
 
 .controller('MoreCtrl', function($scope, $stateParams, Chats) {
   // $scope.chat = Chats.get($stateParams.chatId);
 });
-
