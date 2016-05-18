869e7e9d8925efc8f75b1915cdbf4fdacd4d9a2e
Rahul Patel <rahul.patel@parsus.com>
2016-05-18 13:10:46 -0700

    feat(outages): add report outage modal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,33 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope, $ionicModal) {
+
+  $ionicModal.fromTemplateUrl('templates/report-outage.modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+    // Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
+
+})
 
 .controller('PayBillCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
